abd5d7a7a229aa98b6f1b150741456cf59ca5bfe
ndsurgenor <ndsurgenor@gmail.com>
2026-05-09 10:53:44 +0100

    Adjust text layout on prices page

--- public/js/select.js
+++ public/js/select.js
@@ -225,10 +225,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
             buildTwoColView(sorted, priceContainer, 'Price Order');
 
-            const iconSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-3 h-3 inline-block"><path fill-rule="evenodd" d="M10 1C13.866 1 17 2.79086 17 5C17 7.20914 13.866 9 10 9C6.13401 9 3 7.20914 3 5C3 2.79086 6.13401 1 10 1ZM15.694 9.13079C16.1576 8.86588 16.6044 8.54736 17 8.17775V10C17 12.2091 13.866 14 10 14C6.13401 14 3 12.2091 3 10V8.17775C3.3956 8.54736 3.84244 8.86588 4.30604 9.13079C5.83803 10.0062 7.85433 10.5 10 10.5C12.1457 10.5 14.162 10.0062 15.694 9.13079ZM3 13.1777V15C3 17.2091 6.13401 19 10 19C13.866 19 17 17.2091 17 15V13.1777C16.6044 13.5474 16.1576 13.8659 15.694 14.1308C14.162 15.0062 12.1457 15.5 10 15.5C7.85433 15.5 5.83803 15.0062 4.30604 14.1308C3.84244 13.8659 3.3956 13.5474 3 13.1777Z" clip-rule="evenodd"/></svg>`;
+            const iconSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-3 h-3"><path fill-rule="evenodd" d="M10 1C13.866 1 17 2.79086 17 5C17 7.20914 13.866 9 10 9C6.13401 9 3 7.20914 3 5C3 2.79086 6.13401 1 10 1ZM15.694 9.13079C16.1576 8.86588 16.6044 8.54736 17 8.17775V10C17 12.2091 13.866 14 10 14C6.13401 14 3 12.2091 3 10V8.17775C3.3956 8.54736 3.84244 8.86588 4.30604 9.13079C5.83803 10.0062 7.85433 10.5 10 10.5C12.1457 10.5 14.162 10.0062 15.694 9.13079ZM3 13.1777V15C3 17.2091 6.13401 19 10 19C13.866 19 17 17.2091 17 15V13.1777C16.6044 13.5474 16.1576 13.8659 15.694 14.1308C14.162 15.0062 12.1457 15.5 10 15.5C7.85433 15.5 5.83803 15.0062 4.30604 14.1308C3.84244 13.8659 3.3956 13.5474 3 13.1777Z" clip-rule="evenodd"/></svg>`;
             const key = document.createElement('div');
-            key.className = 'flex flex-col xs:flex-row justify-center items-center gap-3 mb-4 text-xs text-white/85';
-            key.innerHTML = `<span class="flex items-center gap-1.5"><span class="text-red-400">${iconSvg}</span> Most expensive in group</span><span class="flex items-center gap-1.5"><span class="text-emerald-400">${iconSvg}</span> Least expensive in group</span>`;
+            key.className = 'mb-4 text-xs text-white/85';
+            key.innerHTML = `
+                <div class="xs:hidden flex justify-center items-center gap-1">
+                    <span class="flex items-center text-red-400">${iconSvg}</span><span class="text-white/50 leading-none">/</span><span class="flex items-center text-emerald-400">${iconSvg}</span><span class="ml-2 leading-none translate-y-[0.5px]">Most/Least expensive in group</span>
+                </div>
+                <div class="hidden xs:flex justify-center items-center gap-6">
+                    <span class="flex items-center gap-1.5"><span class="flex items-center text-red-400">${iconSvg}</span><span class="leading-none translate-y-[0.5px]">Most expensive in group</span></span>
+                    <span class="flex items-center gap-1.5"><span class="flex items-center text-emerald-400">${iconSvg}</span><span class="leading-none translate-y-[0.5px]">Least expensive in group</span></span>
+                </div>`;
             priceContainer.insertBefore(key, priceContainer.children[1]);
 
             allGroupSections.forEach(s => s.classList.add('hidden'));
